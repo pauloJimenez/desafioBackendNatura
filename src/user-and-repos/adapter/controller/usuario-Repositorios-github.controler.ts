4a6f542c6655c1a90754c921aa84b35c0d40f54f
Paulo Jimenez <paulo.jimenez@accenture.com>
2024-07-12 18:02:08 -0300

    configuração controller

---
+++ src/user-and-repos/adapter/controller/usuario-Repositorios-github.controler.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { ResponseUsuario } from 'src/user-and-repos/domain/model/response-usuario';
+import { BuscaUsuarioERepositoriosService } from 'src/user-and-repos/domain/service/busca-usuario-repositorios.service';
+
+@Controller('usuario-github')
+export class UsuarioERepositoriosGithubController {
+  constructor(
+    private readonly buscaUsuarioERepositoriosService: BuscaUsuarioERepositoriosService,
+  ) {}
+
+  @Get(':nomeUsuario')
+  async buscaUsuarioERepositorios(
+    @Param('nomeUsuario') nomeUsuario: string,
+  ): Promise<ResponseUsuario> {
+    const response =
+      await this.buscaUsuarioERepositoriosService.buscarUsuarioERepositorios(
+        nomeUsuario,
+      );
+    return response;
+  }
+}
